1f449151db3da7809c033a06309240f637c36595
avelon1A <87087325+avelon1A@users.noreply.github.com>
2022-10-20 12:53:36 +0530

    second commit

--- app/src/main/java/com/project/finalsub/MainActivity.kt
+++ app/src/main/java/com/project/finalsub/MainActivity.kt
@@ -3,13 +3,20 @@ package com.project.finalsub
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.project.finalsub.ui.theme.FinalsubTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +29,60 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+
+                    firstrow(name ="CSVTU NOTES" )
                 }
             }
         }
     }
 }
+@Composable
+fun firstrow(name: String){
+    Column(modifier = Modifier.fillMaxSize(),
 
+
+        horizontalAlignment = Alignment.CenterHorizontally) {
+
+        Greeting(name = " CSVTU NOTES")
+        SimpleImage()
+        SimpleButton("civil")
+        SimpleButton("computer science")
+        SimpleButton("eletrical")
+        SimpleButton("mechincal")
+        SimpleButton("ET&T")
+    }
+
+}
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "$name!",fontSize = 30.sp, color = Color.Blue, fontWeight = FontWeight.Bold)
 }
+@Composable
+fun SimpleButton(branch:String) {
+    Button(
+        onClick = {
+            //your onclick code
+        },
+        border = BorderStroke(1.dp, Color.Red),
+        colors = ButtonDefaults.outlinedButtonColors(contentColor = Color.Red)
+    ) {
+        Text(text = branch, color = Color.DarkGray)
+    }
+}
+@Composable
+fun SimpleImage() {
+    Image(
+        painter = painterResource(id = R.drawable.csvtu),
+        contentDescription = "Andy Rubin",
+        modifier = Modifier.fillMaxWidth()
+    )
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     FinalsubTheme {
-        Greeting("Android")
+
     }
 }
